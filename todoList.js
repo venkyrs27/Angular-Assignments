4196399da853703c2ffe509212c31bfc7c30506f
venkyrs27 <venkyrs27@gmail.com>
2016-08-19 13:25:43 +0530

    Update todoList.js

--- todoList.js
+++ todoList.js
@@ -14,16 +14,9 @@ angular.module('todoApp', [])
     todoList.remaining = function() {
       var count = 0;
       angular.forEach(todoList.todos, function(todo) {
-        count += todo.done ? 0 : 1;
+       if(!todo.done)
+        count++;
       });
       return count;
-    };
- 
-    todoList.delete = function() {
-      var oldTodos = todoList.todos;
-      todoList.todos = [];
-      angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) todoList.todos.push(todo);
-      });
     };
   });
